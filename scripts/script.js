03a780c5a8c1980549ad0ee0f7a89b7c474b2ecf
Joel Terrell <falconreid@hotmail.com>
2020-07-31 23:43:55 -0600

    added UV Index with color changes

--- scripts/script.js
+++ scripts/script.js
@@ -32,66 +32,77 @@ $(document).ready(function () {
     $.ajax({
       url: queryURL,
       method: "GET",
-    }).then(function (response) {
-      console.log(response);
-      console.log(response.coord.lon);
-      console.log(response.coord.lat);
-      cityLon = response.coord.lon;
-      cityLat = response.coord.lat;
-
-      var tempF = parseInt(response.main.temp);
-      var imgIcon = response.weather[0].icon;
-      // set hidden div in html with lat and lon coord to draw from for onecall api query
-      $("#hidden1").text(cityLat);
-      $("#hidden2").text(cityLon);
-
-      $("#Name").text(response.name);
-
-      $("#Date").text("Date: " + new Date().toLocaleDateString());
-      // create variable for icon to feed into the URL
-      $("#weatherIcon > img").attr(
-        "src",
-        "http://openweathermap.org/img/wn/" + imgIcon + "@2x.png"
-      );
-
-      $("#Temp").text("Temperature: " + tempF + " F");
-
-      $("#Humidity").text("Humidity: " + response.main.humidity + "%");
-      $("#WindSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
-      // whatever this is will have to move to the 3rd API call...
-      $("#UVIndex").text("UV Index: " + response.main[3]);
-    });
-
-    let cityLat = $("#hidden1").toString();
-    let cityLon = String.valueOf($("#hidden2"));
-
-    // open weather API call for 5 day forecast
-    const queryURL5Day =
-      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-      cityLat +
-      "&lon=" +
-      cityLon +
-      "&units=imperial&appid=74cf78750ecac686f9f70dee01219c17";
-    console.log(queryURL5Day);
-
-    $.ajax({
-      url: queryURL5Day,
-      method: "GET",
-    }).then(function (response) {
-      // console.log(response);
-      // console.log(cityStr);
-      let results = response.list;
-
-      for (let i = 0; i < results.length; i++) {
-        // let x = i + 8; there are 8 data entries for each day.
-        console.log(response.list[i]);
-        var responseData = response.list[i];
+    })
+      .then(function (response) {
+        console.log(response);
+        console.log(response.coord.lon);
+        console.log(response.coord.lat);
+        cityLon = response.coord.lon;
+        cityLat = response.coord.lat;
 
         var tempF = parseInt(response.main.temp);
-        $("p.card-text.temp").text("Temp: " + tempF + " F");
-      }
-      console.log(tempF);
-    });
+        var imgIcon = response.weather[0].icon;
+        // set hidden div in html with lat and lon coord to draw from for onecall api query
+        $("#hidden1").text(cityLat);
+        $("#hidden2").text(cityLon);
+
+        $("#Name").text(response.name);
+
+        $("#Date").text("Date: " + new Date().toLocaleDateString());
+        // create variable for icon to feed into the URL
+        $("#weatherIcon > img").attr(
+          "src",
+          "http://openweathermap.org/img/wn/" + imgIcon + "@2x.png"
+        );
+
+        $("#Temp").text("Temperature: " + tempF + " F");
+
+        $("#Humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#WindSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+      })
+      .then(function () {
+        let cityLat = $("#hidden1").text();
+
+        let cityLon = $("#hidden2").text();
+
+        // open weather API call for 5 day forecast
+        const queryURL5Day =
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+          cityLat +
+          "&lon=" +
+          cityLon +
+          "&units=imperial&appid=74cf78750ecac686f9f70dee01219c17";
+        console.log(queryURL5Day);
+
+        $.ajax({
+          url: queryURL5Day,
+          method: "GET",
+        }).then(function (response) {
+          // console.log(response);
+          // console.log(cityStr);
+          let results = response.list;
+
+          // UV INDEX Section...
+          $("#UVIndex").text("UV Index: " + response.current.uvi);
+          if (response.current.uvi <= 5) {
+            $("#UVIndex").attr("class", "low");
+          } else if (response.current.uvi > 5 && response.current.uvi <= 7) {
+            $("#UVIndex").attr("class", "med");
+          } else if (response.current.uvi >= 8) {
+            $("#UVIndex").attr("class", "high");
+          }
+
+          for (let i = 0; i < results.length; i++) {
+            // let x = i + 8; there are 8 data entries for each day.
+            console.log(response.list[i]);
+            var responseData = response.list[i];
+
+            var tempF = parseInt(response.main.temp);
+            $("p.card-text.temp").text("Temp: " + tempF + " F");
+          }
+          console.log(tempF);
+        });
+      });
   });
 
   // end of ready statement
